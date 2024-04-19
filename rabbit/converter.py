8c8f09196877cff2196592e1a92ac94e1086d33c
ffserrano42 <ff.serrano42@uniandes.edu.co>
2024-04-18 21:17:31 -0500

    descarga local el archivo y sube el pdf al bucket.

--- rabbit/converter.py
+++ rabbit/converter.py
@@ -1,5 +1,6 @@
 import base64
 import datetime
+from pathlib import Path
 import subprocess
 import pika, sys, os, json,traceback
 import io
@@ -15,7 +16,7 @@ from google.oauth2 import service_account
 from google.cloud import pubsub_v1
 
 RABBITMQ_URL =os.environ.get('RABBITMQ_URL', '34.176.60.161')# colocar la ip privada de la maquina donde esta el rabbit
-DATABASE_URL = os.environ.get('DATABASE', "postgresql://api:Uniandes2025!@34.176.118.146:5433/converter")# colocar la ip privada del servidor de postgress
+DATABASE_URL = os.environ.get('DATABASE', "postgresql://api:Uniandes2025!@34.176.96.127:5433/converter")# colocar la ip privada del servidor de postgress
 engine = create_engine(DATABASE_URL)
 Base = declarative_base()
 Base.metadata.create_all(bind=engine)
@@ -35,6 +36,7 @@ class DocumentModel(Base):
 def get_google_credentials(credentials_file_path):
     credentials = service_account.Credentials.from_service_account_file(credentials_file_path)
     return credentials
+
 def callback2(message):
         data = json.loads(message.data.decode("utf-8"))
         name = data.get("id_book")
@@ -54,20 +56,22 @@ def obtain_pdf(id_document):
                     bucket_name = os.environ.get("BUCKET_NAME", "sc_entrega3_files")
                     bucket = client.bucket(bucket_name)
                     blob = bucket.blob(book.source_filename)
-                    if not os.path.exists("./todelete"):
-                        os.makedirs("./todelete")
-                    blob.download_to_filename("./todelete")                    
+                    path_folder = f'./downloads'
+                    # Create this folder locally if it does not exist
+                    # parents=True will create intermediate directories if they do not exist
+                    Path(path_folder).mkdir(parents=True, exist_ok=True)
+                    blob.download_to_filename(f'{path_folder}/{blob.name}')                    
                     upload_folder = os.environ.get("TOCONVERT","remote_folder/")
                     name_output = re.sub(r'\.(xlsx|pptx|docx|odt)$', '.pdf', book.source_filename)                    
                     source_file_without_first_slash=book.source_file
-                    print(f"soffice --headless --convert-to pdf --outdir {upload_folder} {source_file_without_first_slash}")
+                    print(f"soffice --headless --convert-to pdf --outdir {upload_folder} {path_folder}/{blob.name}")
                     #subprocess.call(['soffice', '--headless', '--convert-to', 'pdf', '--outdir', upload_folder, source_file_without_first_slash])
                     print(f"El archivo {book.source_file} ha sido convertido a PDF correctamente.")
-                    pdf_blob = bucket.blob(f"{upload_folder}/{name_output}")
-                    pdf_blob.upload_from_filename(f"{upload_folder}/{name_output}")
+                    pdf_blob = bucket.blob(f"uploaded_example.pdf")
+                    pdf_blob.upload_from_filename(f"{path_folder}/uploaded_example.pdf")
                     book.converted_datetime = datetime.datetime.now()
                     book.status = "Disponible"
-                    book.pdf_file = f"/{upload_folder}{name_output}"# se debe colocar el '/' al inicio para que el api pueda leerlo
+                    book.pdf_file = f"https://storage.cloud.google.com/{bucket.name}/{pdf_blob.name}"# se debe colocar el '/' al inicio para que el api pueda leerlo
                     db.commit()
                     db.refresh(book)
                     print("Document converted to PDF and saved successfully.")
