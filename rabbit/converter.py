d45e5e3ebc0674b6afd06a304652b1cdf6fe387b
ffserrano42 <ff.serrano42@uniandes.edu.co>
2024-04-19 11:54:46 -0500

    adicion del converter, para que transforme y suba a gcp la informacion

--- rabbit/converter.py
+++ rabbit/converter.py
@@ -15,8 +15,7 @@ from google.cloud import storage
 from google.oauth2 import service_account
 from google.cloud import pubsub_v1
 
-RABBITMQ_URL =os.environ.get('RABBITMQ_URL', '34.176.60.161')# colocar la ip privada de la maquina donde esta el rabbit
-DATABASE_URL = os.environ.get('DATABASE', "postgresql://api:Uniandes2025!@34.176.96.127:5433/converter")# colocar la ip privada del servidor de postgress
+DATABASE_URL = os.environ.get('DATABASE', "postgresql://api:Uniandes2025!@34.176.10.10:5433/converter")# colocar la ip privada del servidor de postgress
 engine = create_engine(DATABASE_URL)
 Base = declarative_base()
 Base.metadata.create_all(bind=engine)
@@ -33,11 +32,12 @@ class DocumentModel(Base):
     status=Column(String,nullable=False)
     upload_datetime=Column(DateTime, nullable=False)
     converted_datetime=Column(DateTime, nullable=True)
+    
 def get_google_credentials(credentials_file_path):
     credentials = service_account.Credentials.from_service_account_file(credentials_file_path)
     return credentials
 
-def callback2(message):
+def callback(message):
         data = json.loads(message.data.decode("utf-8"))
         name = data.get("id_book")
         print(f"{name} ha sido recibido")
@@ -56,25 +56,34 @@ def obtain_pdf(id_document):
                     bucket_name = os.environ.get("BUCKET_NAME", "sc_entrega3_files")
                     bucket = client.bucket(bucket_name)
                     blob = bucket.blob(book.source_filename)
-                    path_folder = f'./downloads'
+                    base_folder="downloads"
+                    path_folder = f'./{base_folder}'
                     # Create this folder locally if it does not exist
                     # parents=True will create intermediate directories if they do not exist
                     Path(path_folder).mkdir(parents=True, exist_ok=True)
-                    blob.download_to_filename(f'{path_folder}/{blob.name}')                    
-                    upload_folder = os.environ.get("TOCONVERT","remote_folder/")
-                    name_output = re.sub(r'\.(xlsx|pptx|docx|odt)$', '.pdf', book.source_filename)                    
-                    source_file_without_first_slash=book.source_file
-                    print(f"soffice --headless --convert-to pdf --outdir {upload_folder} {path_folder}/{blob.name}")
-                    #subprocess.call(['soffice', '--headless', '--convert-to', 'pdf', '--outdir', upload_folder, source_file_without_first_slash])
+                    blob.download_to_filename(f'{path_folder}/{blob.name}')
+                    print(blob.path)
+                    #upload_folder = os.environ.get("TOCONVERT","remote_folder/")
+                    #name_output = re.sub(r'\.(xlsx|pptx|docx|odt)$', '.pdf', book.source_filename)                    
+                    #source_file_without_first_slash=book.source_file
+                    print(f"soffice --headless --convert-to pdf --outdir {base_folder}/ {base_folder}/{blob.name}")
+                    subprocess.call(['soffice', '--headless', '--convert-to', 'pdf', '--outdir', base_folder+"/", base_folder+'/'+blob.name])
                     print(f"El archivo {book.source_file} ha sido convertido a PDF correctamente.")
-                    pdf_blob = bucket.blob(f"uploaded_example.pdf")
-                    pdf_blob.upload_from_filename(f"{path_folder}/uploaded_example.pdf")
+                    pdf_filename = re.sub(r'\.(pptx|docx|xlsx|odt)$', '.pdf', blob.name)
+                    pdf_blob = bucket.blob(pdf_filename)
+                    pdf_blob.upload_from_filename(f"{path_folder}/{pdf_filename}")                    
+                    print(f"El archivo {pdf_filename} ha sido subido a Google Cloud Storage correctamente.")
                     book.converted_datetime = datetime.datetime.now()
                     book.status = "Disponible"
                     book.pdf_file = f"https://storage.cloud.google.com/{bucket.name}/{pdf_blob.name}"# se debe colocar el '/' al inicio para que el api pueda leerlo
                     db.commit()
                     db.refresh(book)
                     print("Document converted to PDF and saved successfully.")
+                    delete_files=os.environ.get("DELETE_FILES","False")
+                    if(delete_files=="True"):
+                        print(f"Eliminando archivos {path_folder}/{blob.name} y {path_folder}/{pdf_filename}")
+                        os.remove(f"{path_folder}/{blob.name}")
+                        os.remove(f"{path_folder}/{pdf_filename}")
                 except Exception as e:
                     excepcion_completa = traceback.format_exc()
                     print(f"Error al convertir el archivo {book.source_file} a PDF:", e)
@@ -93,28 +102,14 @@ def get_db():
             return db
         finally:
             db.close()
-def callback(ch, method, properties, body):
-        data = json.loads(body)
-        name = data.get("id_book")      
-        obtain_pdf(name)
-    #try:
-    #    channel.basic_consume(queue='pdfs',
-    #                        auto_ack=True,
-    #                        on_message_callback=callback)
-    #    print(' [*] Esperando mensajes, oprime CTRL+C para salir')
-    #    channel.start_consuming()
-    #except pika.exceptions.ChannelClosedByBroker:
-    #    print("La cola 'pdfs' no existe. Esperando...")    
 
 def main():
-    #connection = pika.BlockingConnection(pika.ConnectionParameters(RABBITMQ_URL,5672))
-    #channel = connection.channel()
     # Crea un suscriptor para el tópico en GCP
     credentials = get_google_credentials("myfirstproject-417702-6a6d72abcd7b.json")
     subscriber = pubsub_v1.SubscriberClient(credentials=credentials)
     subscription_path = subscriber.subscription_path(os.environ.get("PROJECT_ID","myfirstproject-417702"), os.environ.get("PROJECT_SUSCRIPTION","pdfs-sub"))      
     # Inicia la escucha de mensajes
-    subscriber.subscribe(subscription_path, callback=callback2)
+    subscriber.subscribe(subscription_path, callback=callback)
     print('Esperando mensajes, presiona Ctrl+C para salir')
     import time
     while True:
--- varios/readme.txt
+++ varios/readme.txt
@@ -1,7 +1,7 @@
 Decargar las siguientes imagenes
 docker pull ffserrano42/api:04172024 --> imagen de fastapi
 docker pull ffserrano42/t1_streamlit:02192024--> Imagen del streamlit
-docker pull ffserrano42/consumer:24032024--> Consumidor de la cola
+docker pull ffserrano42/converter:04192024--> Consumidor de la cola
 En este caso NO es necesario descargar la imagen de la base de datos, porque se utiliza el servicio de SQL de GCP
 En este caso NO es neceario instalar redes, ya que cada contenedor, estara en una MV diferente, y se conocen solo por la IP privada
 
@@ -13,7 +13,6 @@ docker ps -a -->obtiene los contenedores (corriendo o detenidos)
 docker rm [id del contenedor] --> elimina el contenedor
 docker rmi [id de la imagen] --> elimina la imagen
 docker tag 1234567890ab mi_app:v1.0--Sirve para poner el tag mi_app:v1.0 a la imagen con id 1234567890ab
--v c:/to_convert:/input sirve para montar una ruta local a uno en la maquina virtual en docker
 
 Comandos linux
 1. sudo -->para ejecutar todo con permisos de admin
@@ -22,44 +21,27 @@ Comandos linux
 5. Para instalar libreoffice--> sudo apt install libreoffice
 6. para conertir un archivo a PDF utilizando libreoffice--> sudo soffice --headless --convert-to pdf --outdir remote_folder/ remote_folder/nombre_archivo.pptx
 
-
 Nuevas variables para configurar en los contenedores:
-1. TOCONVERT -->Ruta local donde se deben escribir los archivos. debe tener el mismo nombre del folder que esta en el servidor del worker y se debe llamar remote_folder/ y debe ser el mismo que se monto el nfs
-En el API el valor TOCONVERT debe iniciar con el / por ejemplo {/remote_folder} en el consumer debe finalizar {remote_folder/}
+1. PROJECT_ID = El nombre del proyecto en GCP
+2. PROJECT_TOPIC = El nombre del topico en PUB/sub
+3. BUCKET_NAME = El nombre del bucket
+4. PROJECT_SUSCRIPTION = el nombre del suscriptor asociado al Topico del PUB/SUB
+5. DELETE_FILES = Le indica al worker que despues de cargar el archivo, debe borrar los archivos temporales locales.
 
 
-ips privadas de GCP fs:
-1. API: 10.194.0.2
-2. worker/rabbitmq: 10.194.0.5
-3. nfs: 10.194.0.4
-4. DB_dev: 10.66.192.9
-
-Pasos para levantar la app por primera vez
+Pasos para levantar la app por primera vez desde los contenedores
 2. docker run --name fast_api  -p 5001:5001 -d --restart unless-stopped -e DATABASE_URL='postgresql://api:Uniandes2025!@10.194.0.14:5433/converter' -e PROJECT_ID=myfirstproject-417702 -e PROJECT_TOPIC=pdfs -e BUCKET_NAME=sc_entrega3_files [id_imagen]
     En las variables de DATABASE_URL debe ir el valor entre ' ya que se tiene el caracter de !
 5. docker run --name streamlit -p 8501:8501 -d --restart unless-stopped  -e API_URL=http://10.194.0.2:5001 [id_imagen]
-6. docker run --name consumer --privileged -e RABBITMQ_URL=10.194.0.5 -e DATABASE='postgresql://api:Uniandes2025!@10.66.192.9:5432/converter' -e TOCONVERT=remote_folder/ -v /home/zedano/remote_folder:/remote_folder [id_imagen]
+6. docker run --name worker -d --restart unless-stopped -e BUCKET_NAME=sc_entrega3_files -e PROJECT_ID=myfirstproject-417702 -e PROJECT_SUSCRIPTION=pdfs-sub -e DATABASE='postgresql://api:Uniandes2025!@34.176.10.10:5433/converter' -e DELETE_FILES=True
         En las variables de DATABASE_URL debe ir el valor entre '' ya que se tiene el caracter de !
-
-
 
 Pasos para levanta la app despues de haber corrido los contenedores
 Antes se deben haber levantado las VMs en el siguiente orden:
     1. darle start al servicio de SQL de GCP
-    2. Levantar la VM del servidor nfs
     3. Levantar la VM del frontend
     4. Levantar la VM del API
     5. Levantar la VM del worker
-
-En cada VM se debe:
-    1. Obtener el listado de todos los contenedores con docker ps -a
-    2. Utilizar el comando docker start [id contenedor] para iniciar el contenedor    
-    3.1 en la maquina worker se debe iniciar el programa en python (Plan B):
-    3.1.1 source venv/bin/activate-->ambiente virtual
-    23.1.2 python converter/converter.py-->archivo a ejecutar
-
-Recomendacion!!!
-1. Antes de levantar el contenedor del Consumer, haber levantado todos los otros contenedores, logerase en la app y dejar un archivo, esto hara que la cola tenga algun mensaje  y asi el consumer quede "activo".
 
 
 Comando para ejecutar el proyecto de API
